321b3a614171761ff32f4ed866d667ff8f846d85
uzmanj <uzman.jacob@gmail.com>
2015-05-26 22:00:46 -0400

    Batman 1

---
+++ BATMAN.rb
@@ -0,0 +1,4 @@
+supervillains = {"Rick Grimes" => {"height" => "6 foot",
+                                    "strengths" => ["Sheriff", "Guns"]
+                                    "weaknesses" => ["Paranoid", "]
+                                    }}
